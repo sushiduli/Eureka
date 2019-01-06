5848d885087253e2022ba565fc718885ba46e05c
sushiduli <1105022841@qq.com>
2019-01-06 13:43:32 +0800

    first commit
    commit demo project

---
+++ cloudDemoClient/src/main/java/com/example/cloudDemoClient/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.example.cloudDemoClient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BaseController {
+    
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        
+        return "test service ";
+    }
+
+}
